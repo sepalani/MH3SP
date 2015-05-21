63d9320ffe526a0abcf9a9dd5a2e62f440526eea
payet_s <sebastien.payet@epitech.eu>
2015-05-21 18:50:13 +0200

    Added: generic DNS server in utils

---
+++ utils/MHTriDNSServer.py
@@ -0,0 +1,34 @@
+import utils.uSocketServer as SocketServer
+
+
+class MHTriDNSServer(SocketServer.UDPServer):
+    """Generic DNS server class for MHTri.
+
+    Empty record will point to server ip.
+    """
+
+    record_a = {
+        'mh.capcom.co.jp': '',
+        'mmh-t1-opn02.mmh-service.capcom.co.jp': '',
+        'mmh-t1-opn03.mmh-service.capcom.co.jp': '',
+        'mmh-t1-opn04.mmh-service.capcom.co.jp': '',
+    }
+
+    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True, record={}):
+        SocketServer.UDPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate)
+        if len(record) > 0:
+            self.record = record
+        else:
+            self.record = self.record_a
+
+    def __len__(self):
+        return len(self.record) 
+
+    def __getitem__(self, key):
+        return self.record[key]
+
+    def __setitem__(self, key, item):
+        self.record[key] = item 
+
+    def __delitem__(self, key):
+        del self.record[key]
