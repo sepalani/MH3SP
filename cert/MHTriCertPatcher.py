0defc3258af8347619f1bb5d45d71fafbe5d43fb
payet_s <sebastien.payet@epitech.eu>
2015-06-23 18:25:26 +0200

    Added: CertPatcher base code

--- cert/MHTriCertPatcher.py
+++ cert/MHTriCertPatcher.py
@@ -18,6 +18,32 @@ class CertPatcher(object):
     CERT_OFF = None
     CERT_LEN = None
 
+    def __init__(self, dol):
+        """Constructor.
+
+        dol - path to the main.dol file
+
+        """
+        self.dol = dol
+
+    def patch_cert(self, crt):
+        """Patch the in-game certificate. Will overwrite original dol file!
+
+        crt - path to the certificate file
+
+        """
+
+        data = b''
+        with open(crt, 'rb') as f:
+            data = f.read(self.CERT_LEN)
+            pad = self.CERT_LEN - len(data)
+            data += pad * b'\0'
+        if len(data) != self.CERT_LEN:
+            raise ValueError("Unable to read the certificate!")
+        with open(self.dol, 'rb+') as f:
+            f.seek(self.CERT_OFF)
+            f.write(data)
+
 
 class JAPCertPatcher(CertPatcher):
     """JAP Certificate Patcher."""
