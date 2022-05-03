f93bacdb0d2c4e1af5d4e0ab991a7ca06e7522c7
InusualZ <wesmoret98@gmail.com>
2022-05-03 18:46:38 -0400

    Notify changes of a circle, when a member are kicked

--- fmp_server.py
+++ fmp_server.py
@@ -843,6 +843,7 @@ class FmpRequestHandler(PatRequestHandler):
         circle.departed = True
 
         count = 0
+        changed = False
         data = b''
         for i, player in circle.players:
             if player.is_circle_standby():
@@ -858,6 +859,7 @@ class FmpRequestHandler(PatRequestHandler):
                 pat_handler.send_packet(PatID4.NtcCircleKick, ntc_circle_kick,
                                         seq)
                 circle.players.remove(i)
+                changed = True
 
         data = struct.pack(">I", count)+data
         data = struct.pack(">H", len(data))+data
@@ -865,6 +867,10 @@ class FmpRequestHandler(PatRequestHandler):
 
         self.server.circle_broadcast(circle, PatID4.NtcCircleMatchStart, data,
                                      seq)
+
+        if changed:
+            circle_index = circle.parent.circles.index(circle) + 1
+            self.sendNtcCircleListLayerChange(circle, circle_index, seq)
 
     def recvReqCircleMatchEnd(self, packet_id, data, seq):
         """ReqCircleMatchEnd packet.
