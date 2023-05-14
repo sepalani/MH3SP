4a26420fec78fb6ee2836859af73c374ccbec13e
Sepalani <sepalani@hotmail.fr>
2023-05-14 16:52:30 +0400

    Convert licences to SPDX tags

--- mh/equipment_utils.py
+++ mh/equipment_utils.py
@@ -1,23 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Equipment utils module.
-
-    Monster Hunter 3 Server Project
-    Copyright (C) 2023  Ze SpyRo
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# SPDX-FileCopyrightText: Copyright (C) 2023 MH3SP Server Project
+# SPDX-License-Identifier: AGPL-3.0-or-later
+"""Equipment utils module."""
 
 import struct
 
